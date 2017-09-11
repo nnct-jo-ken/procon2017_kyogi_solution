9e0a63798078d7cfc0e817a762b8475851f6ec3b
shirry0129 <shimizy35@gmail.com>
2017-09-11 13:25:28 +0900

    first commit add acquisition piece shape from JSON

---
+++ shimiz_solution/shimiz_solution/Main.cpp
@@ -0,0 +1,58 @@
+﻿# include <Siv3D.hpp>
+
+String GetStringType(JSONValue::ValueType type) {
+	switch(type){
+	case JSONValue::ValueType::Array:
+		return L"Array";
+	case JSONValue::ValueType::Boolean:
+		return L"Boolean";
+	case JSONValue::ValueType::Null:
+		return L"Null";
+	case JSONValue::ValueType::Number:
+		return L"Number";
+	case JSONValue::ValueType::Object:
+		return L"Object";
+	case JSONValue::ValueType::String:
+		return L"String";
+	default:
+		return L"other";
+	}
+}
+
+void Main() {
+	Window::Resize(1280, 720);
+
+	const JSONReader json(L"data.json");
+
+	Array<Vec2> nodes;
+	Array<Array<Vec2>> pieces;
+
+	int i = 0;
+
+	for (const auto a : json[L"sy"].getArray()) {
+		for (const auto node : a[L"points"].getArray()) {
+			nodes.push_back({ node[L"x"].get<int>(),node[L"y"].get<int>() });
+		}
+		pieces.push_back(nodes);
+		nodes.clear();
+	}
+
+	for (auto &nodes : pieces) {
+		for (auto &node : nodes) {
+			node.x *= 5;
+			node.y *= 5;
+		}
+	}
+
+	while (System::Update()) {
+		if (i >= pieces.size()) {
+			i = 0;
+		}
+
+		Polygon(pieces[i]).draw(10, 10);
+
+		if (System::FrameCount() % 180 == 0) {
+			i++;
+		}
+	}
+}
